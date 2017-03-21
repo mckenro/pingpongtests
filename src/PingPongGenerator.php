cb6f03075cd4313abcb85bf50693f8ab4b0bcf5b
Rob McKenzie <mckenro@gmail.com>
2017-03-20 20:15:32 -0700

    add function countUp

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -3,14 +3,21 @@
     {
         function countUp($input_number)
         {
-            // $i = 1;
-            // while ($i < $input_number) {
-            //   echo $i;
-            //   $i++;
-            // }
+            $result_array = array();
             $i = 1;
-            if (($i > $input_number) && ($i != 0))
-            return false;
+            while ($i < $input_number) {
+              $i++;
+              $push_me = $i;
+              if ($i % 3 == 0) {
+                $push_me = "ping";
+              } else if ($i % 5 ==0) {
+                $push_me "pong";
+              } else if ($i % 15 == 0) {
+                $push_me "ping-pong";
+              }
+              array_push($result_array, $i);
+            }
+            return $result_array;
         }
     }
  ?>
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ "src/PingPongGenerator.php";
+    require_once __DIR__.'/../src/PingPongGenerator.php';
 
     class PingPongGeneratorTest extends PHPUnit_Framework_TestCase
     {
@@ -13,7 +13,31 @@
             $result = $test_countUp->countUp($input);
 
             //Assert
-            $this-assertEquals(!0, $result);
+            $this->assertEquals(false, $result);
+        }
+        function test_numberDivThree()
+        {
+            //Arrange
+            $test_countUp = new PingPongGenerator;
+            $input = 33;
+
+            //Act
+            $result = $test_countUp->countUp($input);
+
+            //Assert
+            $this->assertEquals('ping', $result);
+        }
+        function test_numberDivFive()
+        {
+            //Arrange
+            $test_countUp = new PingPongGenerator;
+            $input = 33;
+
+            //Act
+            $result = $test_countUp->countUp($input);
+
+            //Assert
+            $this->assertEquals('pong', $result);
         }
     }
  ?>
